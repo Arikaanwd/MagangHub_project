fcac0483a43147f41100978ae963dccac4c82d6d
Arikaanwd <norma.wahyu08@gmail.com>
2026-05-08 14:21:29 +0700

    update database

--- database.py
+++ database.py
@@ -1,25 +1,27 @@
 import mysql.connector
 import streamlit as st
 
-#connection
+# connection
 def get_connection():
     return mysql.connector.connect(
-        host="mysql.dashboardmonitoringaset.my.id",
-        user="dashbo50_opset_user",
-        password="8vzL@tCfJYbrL5j",
-        database="dashbo50_db_opset"
+        host=st.secrets["DB_HOST"],
+        port=st.secrets["DB_PORT"],
+        user=st.secrets["DB_USER"],
+        password=st.secrets["DB_PASSWORD"],
+        database=st.secrets["DB_NAME"]
     )
 
 def fetch(query):
     conn = get_connection()
     cursor = conn.cursor()
+
     try:
         cursor.execute(query)
         data = cursor.fetchall()
         columns = [col[0] for col in cursor.description]
+
     finally:
         cursor.close()
         conn.close()
 
     return data, columns
-
